5e122cc238e6c171b638ebe7ae10942db47493f2
dawidg81 <dawidgorski.m@gmail.com>
2025-12-12 23:31:44 +0100

    Build up to date

--- src/version.hpp
+++ src/version.hpp
@@ -2,10 +2,10 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-#define VERSION "0.0.0.0"
+#define VERSION "0.0.0.67"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_COMMIT 0
+#define VERSION_COMMIT 67
 
 #endif // VERSION_HPP
