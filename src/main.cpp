466dc3d63554da237ea43b42fc6ed83630d600a7
dawidg81 <dawidgorski.m@gmail.com>
2025-12-12 14:25:53 +0100

    Beginning to handle player identification packet

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <netinet/in.h>
+#include <string>
 using namespace std;
 
 int main(){
@@ -23,20 +24,48 @@ int main(){
     while(true){
         int clientSocket = accept(serverSocket, nullptr, nullptr);
 
-        uint8_t playerId[131];
-        ssize_t bytesReceived = recv(clientSocket, playerId, sizeof(playerId) - 1, 0);
+        uint8_t playerIdBuf[131];
+        ssize_t bytesReceived = recv(clientSocket, playerIdBuf, sizeof(playerIdBuf) - 1, 0);
 
         if(bytesReceived > 0){
-            cout << "Incoming connection. Received " << bytesReceived << " bytes." << endl;
+            cout << "Incoming connection. Received " << bytesReceived << " bytes. Printing player identification buffer." << endl;
             cout << "HEX: ";
             for(ssize_t i = 0; i < bytesReceived; i++){
-                printf("%02x ", playerId[i]);
+                printf("%02x ", playerIdBuf[i]);
             }
             cout << endl;
-            playerId[bytesReceived] = '\0';
-            cout << "ASCII: '" << (char*)playerId << "'" << endl;
+            playerIdBuf[bytesReceived] = '\0';
+            cout << "ASCII: '" << (char*)playerIdBuf << "'" << endl;
+        }
+
+        struct playerIdPack{
+            uint8_t packet_id;
+            uint8_t prot_ver;
+            string username;
+            string ver_key;
+            uint8_t unused;
+        }
+
+        playerIdPack playerId;
+        playerId.packet_id = playerIdBuf[0];
+        playerId.prot_ver = playerIdBuf[1];
+        // TODO: string username: how to put a set of bytes from playerIdBuf table into string?
+        // TODO: string ver_key: how to put a set of bytes from playerIdBuf table into string?
+        playerId.unused = playerIdBuf[130];
+
+        if(playerId.packet_id == 0x00){
+            cout << "Player identification packet id OK\n";
+        } else {
+            cout << "Player identification packet id ERROR: expected packet id with value 0x00 but got " << playerId.packet_id << ". Closing connection.\n";
+            close(clientSocket);
+        }
+
+        if(playerId.prot_ver == 0x07){
+            cout << "Player identification protocol version OK\n";
+        } else {
+            cout << "Player identification protocol version ERROR: expected protocol version 0x07 but got " << playerId.prot_ver << ". Closing connection.\n";
+            close(clientSocket);
         }
     }
-
     return 0;
 }
