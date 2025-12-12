0111d58bc9bebc302368d5de6d3cc62dad1669ac
dawidg81 <dawidgorski.m@gmail.com>
2025-12-12 23:42:01 +0100

    Update version header to 0.0.0.69

--- src/version.hpp
+++ src/version.hpp
@@ -2,10 +2,10 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-#define VERSION "0.0.0.68"
+#define VERSION "0.0.0.69"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_COMMIT 68
+#define VERSION_COMMIT 69
 
 #endif // VERSION_HPP
