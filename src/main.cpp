b4015777c2d171de085636760b9b56e14976f5e3
dawidg81 <dawidgorski.m@gmail.com>
2025-12-12 22:46:04 +0100

    experimental heartbeat sending function

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,39 @@
 #include <string>
 #include <cstring>
 #include <unistd.h>
+#include <curl/curl.h>
 using namespace std;
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+bool sendHeartbeat(const string& name, int port, int users, int max, const string& salt, bool isPublic) {
+    CURL* curl = curl_easy_init();
+    if(!curl) return false;
+
+    string response;
+    string url = "http://www.classicube.net/server/heartbeat?name=" + name + 
+                 "&port=" + to_string(port) + 
+                 "&users=" + to_string(users) + 
+                 "&max=" + to_string(max) + 
+                 "&salt=" + salt + 
+                 "&public=" + (isPublic ? "true" : "false");
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    CURLcode res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    if(res == CURLE_OK) {
+        cout << "Heartbeat sent. Server URL: " << response << endl;
+        return true;
+    }
+    return false;
+}
 
 int main(){
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
